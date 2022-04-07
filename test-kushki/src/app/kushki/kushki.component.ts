f78cec692174847d04d2c2e7a6c7b91af6b2853f
Daniel Gomez <DaniielKazth@hotmail.com>
2022-04-07 04:43:04 -0500

    Request a card token - OK

--- test-kushki/src/app/kushki/kushki.component.ts
+++ test-kushki/src/app/kushki/kushki.component.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { Kushki } from '@kushki/js';
+import { TokenResponse } from '@kushki/js/lib/types/remote/token_response';
+
 
 @Component({
   selector: 'app-kushki',
   templateUrl: './kushki.component.html',
   styleUrls: ['./kushki.component.css']
 })
+
 export class KushkiComponent implements OnInit {
-  
-  kushki = new Kushki({
-    merchantId: 'public-merchant-id', 
-    inTestEnvironment: true,
-    regional:false
-  });
+
+  cardnumber:string = "5451951574925480";
+  fullname:string = "";
+  mm:string = "";
+  yy:string = "";
+  cvc:string = "";
+  mount:number = 0
+
+  getToken():void{
+    kushki.requestToken({
+      amount: this.mount,
+      currency: "COP",
+      card: {
+        name: this.fullname,
+        number: this.cardnumber,
+        cvc: this.cvc,
+        expiryMonth: this.mm,
+        expiryYear: this.yy,
+      },
+    }, (response:any) => {
+      if(typeof response.code === 'undefined'){
+        console.log("OK-");
+        console.log(response);
+      } else {
+        console.error('Error: ',response.error, 'Code: ', response.code, 'Message: ',response.message);
+      }
+    });
+  }
+
+  checkProperty(object:any, property:any):any{
+    return typeof object !== 'undefined' && object.hasOwnProperty(property);
+  }
 
   constructor() { }
 
@@ -20,3 +49,7 @@ export class KushkiComponent implements OnInit {
   }
 
 }
+const kushki = new Kushki({
+  merchantId: 'e48d1ea9047b4ee8a28f3ddcbb76ff50', 
+  inTestEnvironment: true
+});
